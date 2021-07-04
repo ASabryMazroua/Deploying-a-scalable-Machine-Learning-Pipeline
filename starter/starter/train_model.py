359b8820972ddfdb5531048c80a840dc903aecd8
Ahmed Sabry <a.sabrymazroua@hotmail.com>
2021-07-04 17:13:59 +0200

    minor updates

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -4,11 +4,11 @@ from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
 import pandas as pd
-from ./ml.data import process_data
-from ./ml.model import train_model
+from ml.data import process_data
+from ml.model import train_model
 
 # Add code to load in the data.
-data = pd.read_csv(r"./starter/data/census_clean.csv")
+data = pd.read_csv(r"./data/census_clean.csv")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -31,4 +31,4 @@ X_train, y_train, encoder, lb = process_data(
 
 # Train and save a model.
 model = train_model(X_train, y_train)
-pd.to_pickle(model, "./starter/model/model.pkl")
+pd.to_pickle(model, "./model/model.pkl")
