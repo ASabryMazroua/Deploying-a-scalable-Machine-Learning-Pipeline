4531a5edd2bf67c6ee7ed01c419ab4137dd55522
Ahmed Sabry <a.sabrymazroua@hotmail.com>
2021-07-05 17:37:02 +0200

    Building the Local API

--- starter/main.py
+++ starter/main.py
@@ -10,54 +10,42 @@ from pydantic import BaseModel
 from typing import List
 #Import the inference function to be used to predict the values
 from starter.starter.ml.model import inference
-from starter.stater.ml.data import process_data
+from starter.starter.ml.data import process_data
 #Import the model to be used to predict
 model = pd.read_pickle(r"starter/model/model.pkl")
+Encoder = pd.read_pickle(r"starter/model/encoder.pkl")
 
 #Initial a FastAPI instance
 app = FastAPI()
 
 #Give Heroku the ability to pull in data from DVC upon app start up.
-#if "DYNO" in os.environ and os.path.isdir(".dvc"):
-#    os.system("dvc config core.no_scm true")
-#    if os.system("dvc pull") != 0:
-#        exit("dvc pull failed")
-#    os.system("rm -r .dvc .apt/usr/lib/dvc")
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 # pydantic models
 class DataIn(BaseModel):
     #The input should be alist of 108 values 
-    age : int
-    workclass : str
-    fnlgt : int
-    education : str 
-    education_num : int
-    marital_status : str
-    occupation : str
-    relationship : str
-    race : str
-    sex : str
-    capital_gain : int
-    capital_loss : int
-    hours_per_week : int
-    native_country : str
+    age : int = 39
+    workclass : str =  "State-gov"
+    fnlgt : int = 77516
+    education : str = "Bachelors"
+    education_num : int = 13
+    marital_status : str = "Never-married"
+    occupation : str = "Adm-clerical"
+    relationship : str = "Not-in-family"
+    race : str = "White"
+    sex : str = "Male"
+    capital_gain : int = 2174
+    capital_loss : int = 0
+    hours_per_week : int = 40
+    native_country : str = "United-States"
 
 class DataOut(BaseModel):
     #The forecast output will be either >50K or <50K 
     forecast: str = "Income > 50k"
-
-def convert(input_row):
-    '''
-    This function is used to convert the list input into an array with a shape of (1, 108)
-    
-    input : input_row 
-    type: list
-    
-    output: a numpy ndarray of shape (1, 108)
-    '''
-    result = np.array(input_row)
-    result = result.reshape(1,108)
-    return result
 
 # routes
 @app.get("/welcome")
@@ -67,11 +55,36 @@ async def welcome():
 
 @app.post("/predict", response_model=DataOut, status_code=200)
 def get_prediction(payload: DataIn):
-    #Reading the input row
-    input_row = payload.input_row
-    #Checing that its length is 108
-    if len(input_row) != 108:
-        raise HTTPException(status_code=400, detail="Data length is not accurate! Please Enter a list of 108 elements")
+    #Reading the input data
+    age = payload.age
+    workclass = payload.workclass
+    fnlgt = payload.fnlgt
+    education = payload.education
+    education_num = payload.education_num
+    marital_status = payload.marital_status
+    occupation = payload.occupation
+    relationship = payload.relationship
+    race = payload.race
+    sex = payload.sex
+    capital_gain = payload.capital_gain
+    capital_loss = payload.capital_loss
+    hours_per_week = payload.hours_per_week
+    native_country = payload.native_country
+    #Converted the inputs into Dataframe to be processed 
+    df = pd.DataFrame([{{"age" : age,
+                        "workclass" : workclass,
+                        "fnlgt" : fnlgt,
+                        "education" : education,
+                        "education-num" : education_num,
+                        "marital-status" : marital_status,
+                        "occupation" : occupation,
+                        "relationship" : relationship,
+                        "race" : race,
+                        "sex" : sex,
+                        "capital-gain" : capital_gain,
+                        "capital-loss" : capital_loss,
+                        "hours-per-week" : hours_per_week,
+                        "native-country" : native_country}}])
     # Process the data with the process_data function.
     cat_features = [
     "workclass",
@@ -83,9 +96,9 @@ def get_prediction(payload: DataIn):
     "sex",
     "native-country",
 ]
-    X_processed, y_processed, encoder, lb = process_data(X, categorical_features=cat_features, training=False)
+    X_processed, y_processed, encoder, lb = process_data(df, categorical_features=cat_features, training=False,encoder=Encoder)
     #Calling the inference function to make a prediction  
-    prediction_outcome = inference(model, convert(input_row))[0]
+    prediction_outcome = inference(model, X_processed)
     
     #Interpreting the prediction for the end user
     if prediction_outcome == 0:
