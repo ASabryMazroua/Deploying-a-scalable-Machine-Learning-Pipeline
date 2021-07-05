80a756f00bad7699e93d7811e219ae8f4be8e64a
Ahmed Sabry <a.sabrymazroua@hotmail.com>
2021-07-06 00:52:15 +0200

    Test Heroku

--- main.py
+++ main.py
@@ -12,9 +12,8 @@ from typing import List
 from starter.starter.ml.model import inference
 from starter.starter.ml.data import process_data
 #Import the model to be used to predict
-print(os.getcwd())
-model = pd.read_pickle(r"starter/model/model.pkl")
-Encoder = pd.read_pickle(r"starter/model/encoder.pkl")
+model = pd.read_pickle(r"model/model.pkl")
+Encoder = pd.read_pickle(r"model/encoder.pkl")
 
 #Initial a FastAPI instance
 app = FastAPI()
